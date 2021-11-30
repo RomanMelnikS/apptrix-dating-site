344905990a8a0c4900a18462f63c6e0e20418f32
RomanMelnikS <melnikovichh@yandex.ru>
2021-11-30 19:31:18 +0500

    Push to Heroku.

--- backend/api_dating_site/urls.py
+++ backend/api_dating_site/urls.py
@@ -1,15 +1,25 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+            settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += [re_path(
+    r'^backend_media/(?P<path>.*)$',
+    serve,
+    {'document_root': settings.MEDIA_ROOT, }),
+]
